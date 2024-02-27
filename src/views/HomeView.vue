b844836558b173de1dff4ffca3f4d99f1e29e196
Mizu <psdk55052004@gmail.com>
2024-02-27 11:08:24 +0800

    新增 文檔預覽渲染後 html2canvas 截圖生成圖片

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,60 +6,47 @@ import VueOfficeDocx from '@vue-office/docx'
 // 引入相关样式
 import '@vue-office/docx/lib/index.css'
 
-import html2canvas from 'html2canvas' // eslint-disable-line
+import html2canvas from 'html2canvas'
 
 // 设置文档网络地址，可以是相对地址
 const docx = ref('http://static.shanhuxueyuan.com/test6.docx')
+const imgUrl = ref('')
 
-function rendered () { // eslint-disable-line
-  nextTick(() => {
-  // 獲取VueOfficeDocx元素
-    const element = document.getElementById('vueOfficeDocx')
-    console.log('element', element)
+// 文檔渲染完成後才透過html2canvas生成圖片
+function renderedHandler () {
+  generateImg()
+}
+const generateImg = () => {
+  nextTick(async () => {
+    const el = document.getElementById('vueOfficeDocx')
+    const options = {
+      width: el.offsetWidth,
+      height: el.offsetHeight,
+      useCORS: true, // 是否尝试使用 CORS 从服务器加载图像
+      allowTaint: false // 是否允许跨源图像污染画布
+    }
 
-    // 創建一個新的canvas元素
-    const canvas = document.createElement('canvas')
-    console.log('canvas', canvas)
-    const context = canvas.getContext('2d')
-    console.log('context', context)
-
-    // 設置canvas尺寸
-    canvas.width = 100
-    canvas.height = 100
-    // canvas.width = element.offsetWidth
-    // canvas.height = element.offsetHeight
-
-    // 將VueOfficeDocx元素內容繪製到canvas上
-    context.drawImage(element, 0, 0, 100, 100)
-
-    // 將canvas轉換為圖片格式
-    const imageData = canvas.toDataURL('image/png')
-    console.log('imageData', imageData)
-
-    // 創建一個新的圖片元素
-    // const image = new Image()
-
-    // 設置圖片源為canvas轉換的圖片資料
-    // image.src = imageData
-
-    // 將圖片元素加入到DOM中，或者進行其他後續處理
-    // document.body.appendChild(image)
+    await html2canvas(el, options).then(async (canvas) => {
+      imgUrl.value = canvas.toDataURL('image/jpeg') // 转base64
+    })
   })
 }
 
 </script>
 
 <template>
-  <button type='button'
-          class='btn btn-primary'
-          @click='generateImg'>
-    下載
-  </button>
 
-  <VueOfficeDocx
-      :src="docx"
-      style="width:100vw;height:100vh;"
-  />
+  <img :src='imgUrl' alt='' style="width:100px;height:100px;">
+
+  <div id="vueOfficeDocx"
+       class="position-absolute"
+       style="transform: translate(-999999px, -999999px);">
+    <VueOfficeDocx
+        :src="docx"
+        @rendered="renderedHandler"
+        style="width:100vw;height:100vh;"
+    />
+  </div>
 
 </template>
 
